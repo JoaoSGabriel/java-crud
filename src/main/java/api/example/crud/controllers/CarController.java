a6ca67f5c1ef1460f4a677a870e1ccccab82f012
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-04-03 11:33:51 -0300

    feat: implement delete route

--- src/main/java/api/example/crud/controllers/CarController.java
+++ src/main/java/api/example/crud/controllers/CarController.java
@@ -3,7 +3,9 @@ package api.example.crud.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import api.example.crud.dto.CarDTO;
 import api.example.crud.models.Car;
 import api.example.crud.repositories.CarRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/car")
@@ -25,7 +28,12 @@ public class CarController {
     }
 
     @PostMapping
-    public void create(@RequestBody CarDTO req) {
+    public void create(@RequestBody @Valid CarDTO req) {
         repository.save(new Car(req));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
